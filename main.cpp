85f96cd82a9a0f103529889830595bd0c5575cae
sdrrv <duartecelvas@gmail.com>
2021-05-15 15:25:12 +0100

    Created the Class Graph

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <cstddef>
 #include <iostream>
 #include <memory>
 #include <vector>
@@ -49,5 +50,41 @@ class Vertex{
       return _adjVertexes.get();
     }
 };
+
+class Graph{
+  private:
+    int _size;
+    std::unique_ptr<std::list<Vertex *>> _Vertexes; // All the vertexes in the graph
+    std::unique_ptr<std::vector<std::vector<int>> > _adjMatrix; // AdjMatrix
+  public:
+    Graph(): _Vertexes(new std::list<Vertex *> ),
+    _adjMatrix(new std::vector<std::vector<int>>){ _size = 0; }
+
+    void setSize(int size){
+      _size = size;
+      _Vertexes->reverse(_size);
+    }
+
+    Vertex *getVertex(int ID){ // Se mudarmos o _Vertexes para um vector podemos aceder a cada vertice em O(1), esta assim porque n quero alterar sem te perguntar primeiro.
+      for (Vertex *vertex : *_Vertexes){
+        if(vertex->getId() == ID)
+          return vertex;
+      }
+      return NULL;
+    }
+    
+    void addVertex(Vertex* vertex){
+      _Vertexes->push_front(vertex);
+    }
+
+    int getMatrixValue(int Linha, int Coluna){
+      return (*_adjMatrix)[Linha][Coluna];
+    }
+};
+
+
+
+
+
 
 

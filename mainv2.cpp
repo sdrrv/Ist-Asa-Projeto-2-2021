bf1b5872a2b4c3d490b38bc12c94d583639fc26b
Duarte Elvas <duartecelvas@gmail.com>
2021-05-16 11:12:14 +0100

    mainV2 updated

--- mainv2.cpp
+++ mainv2.cpp
@@ -6,10 +6,28 @@
 #include <stack>
 #include <string>
 
-struct Vertex{
-  int h, e,
-}
 
-std::vector<int> h;
-std::vector<int> e;
+struct Arc{
+  int _cap,_ID;
+
+  Arc(int cap, int ID){
+    _cap = cap;
+    _ID = ID;
+  }
+};
+
+
+struct Vertex{
+  int h, e;
+  std::list<std::unique_ptr<Arc>> _arcs;
+
+ 
+
+  void addArc(int cap, int id){
+    _arcs.push_front( new std::unique_ptr<Arc> (cap, id));
+  }
+
+};
+
+
 std::vector<std::list>
